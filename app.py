8927391b1fa2248379fc43595d3c11cfbdd85dbf
Deepak16ee07 <deepak.mah205@gmail.com>
2020-06-29 23:17:50 +0530

    final update

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask , jsonify , make_response,request
+from flask_cors import CORS
 import pickle
 import numpy as np
 import os
-imp_model = pickle.load(open('/content/model.pk1','rb'))
-app = Flask(__name__)
-@app.route('/',methods = ["GET"])
-def index():
-  return "server is running..."
-@app.route('/predict', methods = ['POST'])
-def predict():
+
+def predictions(req):
   data = request.get_json(force = True)
-  param1 = int(data["sepallength"])
-  param2 = int(data["sepalwidth"])
-  param3 = int(data["petallength"])
-  param4 = int(data["petalwidth"])
+  param1 = data["sepalLength"]
+  param2 = data['sepalWidth']
+  param3 = data['petalLength']
+  param4 = data['petalWidth']
   pred = np.array([[param1 , param2 ,param3 ,param4]], dtype = np.float)
   y_pred = imp_model.predict(pred)
   print(y_pred[0])
-  return jsonify(results = int(y_pred[0]))
+  return(y_pred[0])
 
+imp_model = pickle.load(open('model.pk2','rb'))
+app = Flask(__name__)
+CORS(app)
+@app.route('/',methods = ["GET","POST"])
+def index():
+  if request.method == 'POST':
+    return jsonify(results = int(predictions(request)))
+  else :
+    return ("server is running...")
+@app.route('/predict', methods = ['GET','POST'])
+def predict():
+  if request.method == 'POST':
+    return jsonify(results = int(predictions(request)))
+  else :
+    return ("server is running..")
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = int(os.environ.get("PORT",8080)))
